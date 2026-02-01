dcf100c35d6ca197ba4529c850c982ee9810b6be
VoidX <voidxb@gmail.com>
2026-02-01 22:29:51 +0100

    Chat formatting

--- src_web/chat.php
+++ src_web/chat.php
@@ -57,20 +57,22 @@ $slm = $time - getAIVar("moa-available") <= $procTimeout;
 }
 ?>
     </div>
-    <div class="card-footer">
-      <?php if (!$offline) { ?>
-      <div class="input-group">
-        <textarea class="form-control" id="input" placeholder="Ask <?=$chatName ?> anything..." autofocus></textarea>
-        <button class="btn btn-danger" id="stop" style="display:none;" onclick="stop()">Stop</button>
-        <button class="btn btn-primary" id="send" onclick="send()">Send</button>
-      </div>
-      <?php if ($uid) { ?>
-      <p class="text-center mt-3"><small><?=$chatName ?> can make mistakes. Check important info.</small></p>
-      <?php } else { ?>
-      <p class="text-center mt-3"><small>By chatting with <?=$chatName ?>, you state that you have read the <a href="tos.php">Terms of Service</a> and the <a href="gdpr.php">Privacy Policy</a>, and agree to both.</small></p>
-      <?php }
-      addon("chat_footer");
-      } ?>
+	<div class="card-footer">
+	<?php if (!$offline) { ?>
+		<div class="input-group">
+			<textarea class="form-control" id="input" placeholder="Ask <?=$chatName ?> anything..." autofocus></textarea>
+			<button class="btn btn-danger" id="stop" style="display:none;" onclick="stop()">Stop</button>
+			<button class="btn btn-primary" id="send" onclick="send()">Send</button>
+		</div>
+		<p class="text-center small m-2"style="font-size: 0.7rem">
+		<?php if ($uid) { ?>
+			<?=$chatName ?> can make mistakes. Check important info.
+		<?php } else { ?>
+			By chatting with <?=$chatName ?>, you state that you have read the <a href="tos.php">Terms of Service</a> and the <a href="gdpr.php">Privacy Policy</a>, and agree to both.
+		<?php } ?>
+		</p>
+		<?php addon("chat_footer");
+	} ?>
     </div>
   </div>
 </div>
